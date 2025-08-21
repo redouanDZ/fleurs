3814e9b961e662d4344ca3defeebea0e7bb6a88a
redouanDZ <redhkobz@gmail.com>
2025-08-21 14:40:03 +0200

    update netlify

--- .netlify/functions/imagekit-auth.js
+++ .netlify/functions/imagekit-auth.js
@@ -1,19 +1,7 @@
+
 const ImageKit = require("imagekit");
 
 exports.handler = async function (event, context) {
-  // Handle preflight OPTIONS request
-  if (event.httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-      },
-      body: ""
-    };
-  }
-
   try {
     const imagekit = new ImageKit({
       publicKey: "public_aF1VLWznWZonCwPUp2VzFSZFFjw=",
@@ -25,23 +13,11 @@ exports.handler = async function (event, context) {
 
     return {
       statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(authenticationParameters),
     };
   } catch (err) {
-    return { 
-      statusCode: 500, 
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type"
-      },
-      body: JSON.stringify({ error: err.message }) 
-    };
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
   }
 };
+
